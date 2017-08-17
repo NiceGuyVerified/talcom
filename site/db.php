fcd9edf36e67549e2ea2d35e52096a73dc5be297
niceguyverified <overratedgamingyt@gmail.com>
2017-08-17 14:56:43 -0600

    i changed the signup to be more secure and attempted to make login page work still working on it

--- site/db.php
+++ site/db.php
@@ -1,6 +1,3 @@
 <?php
-    $mysqli = new mysqli("158.69.215.252", "talcom", "TalcomDev", "talcom");
-    if ($mysqli->connect_error) {
-        die("Connection failed: " . $mysqli->connect_error);
-    }
+    $link = mysqli_connect("158.69.215.252", "talcom", "TalcomDev", "talcom") or die($link);
 ?>
--- site/signup_parse.php
+++ site/signup_parse.php
@@ -5,12 +5,18 @@
 include 'db.php';
     if(isset($_POST['submit'])) {
         if($_POST['pass1'] === $_POST['pass2']) {
-            $sql = "INSERT INTO users (username, email, password) VALUES ('" . $_POST['username'] . "', '" . $_POST['email'] . "', '" . $_POST['pass1'] . "')";
+            $escapedName = mysqli_real_escape_string($link, $_POST['username']);
+            $escapedPW = mysqli_real_escape_string($link, $_POST['pass1']);
+            $salt = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
+            $saltedPW =  $escapedPW . $salt;
+            $hashedPW = hash('sha256', $saltedPW);  
+            
+            $sql = "INSERT INTO users (username, email, password, salt) VALUES ('$escapedName', '" . $_POST['email'] . "', '$hashedPW', '$salt')";
 
-            if ($mysqli->query($sql) === TRUE) {
+            if ($link->query($sql) === TRUE) {
                 echo "Successfully Signed Up!";
                 } else {
-                echo "Error: " . $sql . "<br>" . $mysqli->error;
+                echo "Error: " . $sql . "<br>" . $link->error;
             }
         } else {
             echo "Passwords do not match";
